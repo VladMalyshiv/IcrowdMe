a42f0d323c3304a3a6d43cd3ee167d8b99dd4c41
Влад Малышев <79473986+VladMalyshiv@users.noreply.github.com>
2021-10-26 19:37:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.banner_slider').slick({
+		prevArrow:'<button  class="slick-prev"><img src="images/arrow-left.png"></button>',
+		nextArrow:'<button  class="slick-next"><img src="images/arrow-right.png"></button>',
+	});
+})
+        
+        
